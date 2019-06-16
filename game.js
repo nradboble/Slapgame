7494384eaeef05f286b6228b56127ec261b542bc
nradboble <nradboble@gmail.com>
2019-06-16 01:20:32 -0600

    p1

--- game.js
+++ game.js
@@ -1,1 +1,33 @@
-q
+var health = 100;
+var playerName = "";
+var hits = 0;
+
+
+function namePrompt() {
+  playerName = prompt("What is your name?");
+  update();
+}
+
+function slap() {
+  health = (health - 1);
+  hits = (hits + 1);
+  update()
+}
+function punch() {
+  health = (health - 5);
+  hits = (hits + 1);
+  update()
+}
+function kick() {
+  health = (health - 10);
+  hits = (hits + 1);
+  update()
+}
+
+function update() {
+  document.getElementById("health").innerText = " " + health;
+  document.getElementById("name").innerText = playerName;
+  document.getElementById("hits").innerText = hits;
+}
+
+update()
